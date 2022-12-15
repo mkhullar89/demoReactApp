b0e0d9644d4682e44efa784a72d42a8b991804cd
Manav Khullar <mkhullar89@gmail.com>
2022-12-15 10:31:45 +0530

    Added DummyReact App and namingConventionFile

---
+++ README.md
@@ -0,0 +1,6 @@
+# Getting Started with Create React App
+### First Write Demo
+
+This is the [`Demo App I am Writing` ]
+
+## Trying to underStand How README Works
---
+++ nameConvention.cs
@@ -0,0 +1,13 @@
+public class Student{ 
+    private readonly int _numberTwo;
+    public int NumberThree{get;set;};
+    constructor(int numberOne,int numberTwo ){
+        int numberInitialize=numberOne
+    }
+}
+
+public interface IStudent{
+    public string Name{get;set;};
+    public int salary {get;set;};
+    public string Address{get;set;};
+}
