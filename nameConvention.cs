c7c372482cc457cd804e602f1efbac547ceb4fc3
Manav Khullar <mkhullar89@gmail.com>
2022-12-15 14:15:52 +0530

    Added Some Files To React Project and Changes to nameConventions To Try Branching

--- nameConvention.cs
+++ nameConvention.cs
@@ -1,8 +1,10 @@
 public class Student{ 
     private readonly int _numberTwo;
     public int NumberThree{get;set;};
+    public int NumberTwo;
     constructor(int numberOne,int numberTwo ){
         int numberInitialize=numberOne
+        NumberTwo=numberTwo
     }
 }
 
@@ -10,4 +12,5 @@ public interface IStudent{
     public string Name{get;set;};
     public int salary {get;set;};
     public string Address{get;set;};
+    public string testingDummyBranch {get;set;}
 }
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <div className="jumbotron">
+            <p className="text-primary text-center">
+                This is Home Page
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
